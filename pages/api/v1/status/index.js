9a27acf803b05cdeb56e34ce2b86c6484c8818c8
devLucasMoraes <devlucasmoraes@gmail.com>
2024-11-23 16:04:33 -0300

    Implementa endpoint de status com informações do banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,28 @@
 import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1");
-  console.log(result.rows);
-  res.status(200).json({ status: "ok" });
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConectionsValue = dbOpenedConectionsResult.rows[0].count;
+
+  const updateAt = new Date().toISOString();
+  res.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenedConectionsValue,
+      },
+    },
+  });
 }
